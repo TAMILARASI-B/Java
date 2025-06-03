2638c416fd716d5b2c4c2f5fc32ee94ea11b80d3
TAMILARASI-B <167201090+TAMILARASI-B@users.noreply.github.com>
2025-06-03 10:26:32 +0530

    Add files via upload

---
+++ SealedDemo.java
@@ -0,0 +1,48 @@
+package org;
+
+
+sealed class Human permits Manish, Vartika, Anjali
+{   
+    public void printName()
+    {
+        System.out.println("Default");
+    }
+}
+
+non-sealed class Manish extends Human
+{
+    public void printName()
+    {
+        System.out.println("Manish Sharma");
+    }
+}
+
+non-sealed class Vartika extends Human
+{
+    public void printName()
+    {
+        System.out.println("Vartika Dadheech");
+    }
+}
+
+final class Anjali extends Human
+{
+    public void printName()
+    {
+        System.out.println("Anjali Sharma");
+    }
+}
+
+public class SealedDemo
+{
+    public static void main(String[] args)
+    {   
+        Human h1 = new Anjali();
+        Human h2 = new Vartika();
+        Human h3 = new Manish();
+        
+        h1.printName();
+        h2.printName();
+        h3.printName();
+    }
+}
